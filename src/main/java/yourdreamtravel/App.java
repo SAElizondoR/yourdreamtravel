2e2990d2048d48ec69fbb7094c68250ec9457289
Cheko <sergio.elizondord@uanl.edu.mx>
2022-11-29 10:36:34 +0100

    First commit

---
+++ src/main/java/yourdreamtravel/App.java
@@ -0,0 +1,17 @@
+package yourdreamtravel;
+
+/**
+ * App
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Main
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+      // TODO document why this method is empty
+    }
+}
