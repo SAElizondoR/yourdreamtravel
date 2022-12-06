87b9d178efeb95b8441484479399387b3c157bb2
TheFlux269 <theflux269@gmail.com>
2022-12-06 10:32:45 +0100

    replace console()

--- src/main/java/yourdreamtravel/ui/TextUI.java
+++ src/main/java/yourdreamtravel/ui/TextUI.java
@@ -27,7 +27,7 @@ public class TextUI {
     public void run() {
         int choix = 0;
         do {
-            System.console().printf("\nSÉLECTIONNER UNE OPTION\n");
+            System.out.printf("\nSÉLECTIONNER UNE OPTION\n");
             choix = choisirOption(Arrays.asList("Créer client", "Selectionner un client",
                 "Quitter le programme"));
             switch (choix) {
@@ -43,16 +43,16 @@ public class TextUI {
     }
 
     private void creerClient() {
-        System.console().printf("CRÉER CLIENT\n");
-        System.console().printf("Entrez le nom: ");
+        System.out.printf("CRÉER CLIENT\n");
+        System.out.printf("Entrez le nom: ");
         String nom = lireChaine();
         agenceService.addClient(nom);
-        System.console().printf("Client ajouté!\n");
+        System.out.printf("Client ajouté!\n");
     }
 
     private void selectionnerClient() {
         Map<String, Client> clients = agenceService.getClientsMap();
-        System.console().printf("SÉLECTIONNER CLIENT\n");
+        System.out.printf("SÉLECTIONNER CLIENT\n");
         List<String> clientNames = new ArrayList<>(clients.keySet());
         int index = choisirOption(clientNames);
         agenceService.setClientActif(clients.get(clientNames.get(index - 1)));
@@ -60,7 +60,7 @@ public class TextUI {
     }
 
     private void clientMenu() {
-        System.console().printf("MENU DU CLIENT\n");
+        System.out.printf("MENU DU CLIENT\n");
         int choix = choisirOption(Arrays.asList("Faire une réservation de voyage",
             "Quitter le menu du client"));
         if (choix == 1)
@@ -68,32 +68,32 @@ public class TextUI {
     }
 
     private void faireReservationVoyage() {
-        System.console().printf("FAIRE UNE RÉSERVATION DE VOYAGE\n");
+        System.out.printf("FAIRE UNE RÉSERVATION DE VOYAGE\n");
 
         Map<String, Lieu> destinations = agenceService.getDestinationMap();
-        System.console().printf("Sélectionnez le lieu de départ:\n");
+        System.out.printf("Sélectionnez le lieu de départ:\n");
         List<String> destinationNames = new ArrayList<>(destinations.keySet());
         int index = choisirOption(destinationNames);
         Lieu depart = destinations.get(destinationNames.get(index - 1));
         destinationNames.remove(index - 1);
 
-        System.console().printf("Sélectionnez la destination:\n");
+        System.out.printf("Sélectionnez la destination:\n");
         index = choisirOption(destinationNames);
         Lieu destination = destinations.get(destinationNames.get(index - 1));
-        System.console().printf("Itinéraire proposé:\n%s\n", String.join("\n",
+        System.out.printf("Itinéraire proposé:\n%s\n", String.join("\n",
             agenceService.proposerItineraire(depart, destination)));
 
         index = choisirOption(Arrays.asList("Accepter", "Réfuser"));
         if (index != 1)
             return;
-        
+
         Map<String, Calendar> dates = agenceService.getDatesPossibles();
-        System.console().printf("Sélectionnez la date de départ:\n");
+        System.out.printf("Sélectionnez la date de départ:\n");
         List<String> dateStrings = new ArrayList<>(dates.keySet());
         index = choisirOption(dateStrings);
         agenceService.setDateReservation(dates.get(dateStrings.get(index)));
 
-        System.console().printf("Sélecionnez le type de service:\n");
+        System.out.printf("Sélecionnez le type de service:\n");
         index = choisirOption(Arrays.asList("Service simple", "Service haute gamme", "Sans service"));
         switch (index) {
             case 1:
@@ -118,14 +118,14 @@ public class TextUI {
 
     private void faireReservationHotel() {
         Map<String, Hotel> hotels = agenceService.getHotelMap();
-        System.console().printf("FAIRE UNE RÉSERVATION D'HOTEL\n");
+        System.out.printf("FAIRE UNE RÉSERVATION D'HOTEL\n");
         List<String> hotelNames = new ArrayList<>(hotels.keySet());
         int index = choisirOption(hotelNames);
     }
 
     private void locationDeVoiture() {
         Map<String, LoueurVoiture> loueurs = agenceService.getLoueurMap();
-        System.console().printf("LOCATION DE VOITURE\n");
+        System.out.printf("LOCATION DE VOITURE\n");
         List<String> loueurNames = new ArrayList<>(loueurs.keySet());
         int index = choisirOption(loueurNames);
     }
@@ -133,14 +133,14 @@ public class TextUI {
     private int choisirOption(List<String> options) {
         int compteur = 1;
         for (String option: options)
-            System.console().printf("%d) %s\n", compteur++, option);
-        System.console().printf("Votre choix: ");
+            System.out.printf("%d) %s\n", compteur++, option);
+        System.out.printf("Votre choix: ");
         try {
             int choix = Integer.parseInt(lireChaine());
-            System.console().printf("\n");
+            System.out.printf("\n");
             return choix;
         } catch (NumberFormatException e) {
-            System.console().printf("Entrez un numéro.\n");
+            System.out.printf("Entrez un numéro.\n");
         }
         return 0;
     }
@@ -149,7 +149,7 @@ public class TextUI {
         try {
             return buff.readLine();
         } catch (IOException e) {
-            System.console().printf("Impossible de lire l'entré.\n");
+            System.out.printf("Impossible de lire l'entré.\n");
         }
         return null;
     }
